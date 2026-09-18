3a75c5b0cba1a1d6dc93a671097552d41a81671b
Mehmet cerdik457 <224373170+mehmetcerdik457-ctrl@users.noreply.github.com>
2026-09-19 02:42:41 +0300

    P1: patch vulnerable Netty build classpath and automate recovery backup

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,1 +1,15 @@
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        constraints {
+            classpath("io.netty:netty-handler:4.1.137.Final") {
+                because("Patches current critical/high Netty build-classpath advisories while AGP 8.5.2 remains in use")
+            }
+        }
+    }
+}
+
 plugins { id("com.android.application") version "8.5.2" apply false; kotlin("android") version "1.9.24" apply false }
