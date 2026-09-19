086a0521b6ba333e8d1d4912b90f62029670c1ee
Mehmet cerdik457 <224373170+mehmetcerdik457-ctrl@users.noreply.github.com>
2026-09-19 10:34:11 +0300

    security: force patched JDOM 2.0.6.1 in settings classpath

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,7 +15,8 @@ buildscript {
     configurations.getByName("classpath") {
         resolutionStrategy.force(
             "commons-io:commons-io:2.14.0",
-            "org.bitbucket.b_c:jose4j:0.9.6"
+            "org.bitbucket.b_c:jose4j:0.9.6",
+            "org.jdom:jdom2:2.0.6.1"
         )
     }
 }
