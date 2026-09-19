faccea5bde735bfa2937c34c1c3245d097da6a47
Mehmet cerdik457 <224373170+mehmetcerdik457-ctrl@users.noreply.github.com>
2026-09-19 03:07:22 +0300

    P1: update Kotlin security baseline and latest Netty 4.1 security line

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,23 +6,22 @@ buildscript {
     }
     dependencies {
         classpath("com.android.tools.build:gradle:8.5.2")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.24")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.4.20")
 
-        // These constraints are deliberately on the same buildscript classpath
-        // that loads AGP/Kotlin. Gradle's dependency-submission guidance requires
-        // this placement for vulnerable transitive dependencies introduced by plugins.
+        // Build-tool dependencies stay on one patched Netty line. The plugin
+        // classpath is constrained here because this is where AGP/Kotlin load.
         constraints {
-            classpath("io.netty:netty-buffer:4.1.137.Final")
-            classpath("io.netty:netty-codec:4.1.137.Final")
-            classpath("io.netty:netty-codec-http:4.1.137.Final")
-            classpath("io.netty:netty-codec-http2:4.1.137.Final")
-            classpath("io.netty:netty-codec-socks:4.1.137.Final")
-            classpath("io.netty:netty-common:4.1.137.Final")
-            classpath("io.netty:netty-handler:4.1.137.Final")
-            classpath("io.netty:netty-handler-proxy:4.1.137.Final")
-            classpath("io.netty:netty-resolver:4.1.137.Final")
-            classpath("io.netty:netty-transport:4.1.137.Final")
-            classpath("io.netty:netty-transport-native-unix-common:4.1.137.Final")
+            classpath("io.netty:netty-buffer:4.1.138.Final")
+            classpath("io.netty:netty-codec:4.1.138.Final")
+            classpath("io.netty:netty-codec-http:4.1.138.Final")
+            classpath("io.netty:netty-codec-http2:4.1.138.Final")
+            classpath("io.netty:netty-codec-socks:4.1.138.Final")
+            classpath("io.netty:netty-common:4.1.138.Final")
+            classpath("io.netty:netty-handler:4.1.138.Final")
+            classpath("io.netty:netty-handler-proxy:4.1.138.Final")
+            classpath("io.netty:netty-resolver:4.1.138.Final")
+            classpath("io.netty:netty-transport:4.1.138.Final")
+            classpath("io.netty:netty-transport-native-unix-common:4.1.138.Final")
         }
     }
 }
