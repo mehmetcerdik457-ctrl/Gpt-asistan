851f7ab0cf16f711700cf4103ebc4ea6c354bb62
Mehmet cerdik457 <224373170+mehmetcerdik457-ctrl@users.noreply.github.com>
2026-09-19 04:32:46 +0300

    security: pin patched Commons IO in settings classpath

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,6 +6,17 @@ pluginManagement {
     }
 }
 
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+    configurations.getByName("classpath") {
+        resolutionStrategy.force("commons-io:commons-io:2.14.0")
+    }
+}
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
