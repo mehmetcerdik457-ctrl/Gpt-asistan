1900f50c304eda6a9ce68658b7b14861d46207c7
Mehmet cerdik457 <224373170+mehmetcerdik457-ctrl@users.noreply.github.com>
2026-09-19 10:43:48 +0300

    security: force patched commons-compress 1.28.0 in build classpath

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,8 @@ buildscript {
         resolutionStrategy.force(
             "commons-io:commons-io:2.14.0",
             "org.bitbucket.b_c:jose4j:0.9.6",
-            "org.jdom:jdom2:2.0.6.1"
+            "org.jdom:jdom2:2.0.6.1",
+            "org.apache.commons:commons-compress:1.28.0"
         )
     }
     dependencies {
@@ -38,6 +39,7 @@ buildscript {
             classpath("commons-io:commons-io:2.14.0")
             classpath("org.bitbucket.b_c:jose4j:0.9.6")
             classpath("org.jdom:jdom2:2.0.6.1")
+            classpath("org.apache.commons:commons-compress:1.28.0")
         }
     }
 }
